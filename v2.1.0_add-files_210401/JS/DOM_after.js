80a1534fe432373b2979725534748856cad5c996
ripend06 <makoto6760@gmail.com>
2021-04-01 23:38:53 +0900

    v2.1.0_fixed_files_210401

---
+++ v2.1.0_add-files_210401/JS/DOM_after.js
@@ -0,0 +1,104 @@
+/*$(function(){
+    $(window).on('scroll', function() {
+        if($(this).scrollTop() + $(window).height() < $('.b').offset().top){
+            $('.bana').toggleClass('aaa');
+        }
+    });
+});*/
+/*
+$(function(){
+    $('.infiniteslide1').infiniteslide();
+});*/
+
+/* QA開閉 */
+$(function() {
+$('.JS-hide').hide();
+$('.JS-hide-2').hide();
+$('.JS-hide-3').hide();
+$('.JS-hide-4').hide();
+$('.JS-hide-5').hide();
+$('.JS-hide-6').hide();
+$('.JS-hide-7').hide();
+$('.JS-hide-8').hide();
+$('.JS-hide-9').hide();
+$('.JS-hide-10').hide();
+$('.JS-hide-11').hide();
+$('.JS-hide-12').hide();
+$('.JS-hide-13').hide();
+$('.JS-hide-14').hide();
+$('.JS-hide-15').hide();
+$('.JS-hide-16').hide();
+$('.JS-hide-17').hide();
+$('.JS-hide-18').hide();
+$('.JS-hide-19').hide();
+
+
+$('.JS-click').click(function() {
+    $('.JS-rorate').toggleClass('active');
+    $('.JS-hide').slideToggle();
+});
+$('.JS-click-2').click(function() {
+    $('.JS-rorate-2').toggleClass('active');
+    $('.JS-hide-2').slideToggle();
+});
+$('.JS-click-3').click(function() {
+    $('.JS-rorate-3').toggleClass('active');
+    $('.JS-hide-3').slideToggle();
+});
+$('.JS-click-4').click(function() {
+    $('.JS-rorate-4').toggleClass('active');
+    $('.JS-hide-4').slideToggle();
+});
+$('.JS-click-5').click(function() {
+    $('.JS-rorate-5').toggleClass('active');
+    $('.JS-hide-5').slideToggle();
+});
+$('.JS-click-6').click(function() {
+    $('.JS-rorate-6').toggleClass('active');
+    $('.JS-hide-6').slideToggle();
+});
+$('.JS-click-7').click(function() {
+    $('.JS-rorate-7').toggleClass('active');
+    $('.JS-hide-7').slideToggle();
+});
+$('.JS-click-8').click(function() {
+    $('.JS-rorate-8').toggleClass('active');
+    $('.JS-hide-8').slideToggle();
+});
+$('.JS-click-9').click(function() {
+    $('.JS-rorate-9').toggleClass('active');
+    $('.JS-hide-9').slideToggle();
+});
+$('.JS-click-10').click(function() {
+    $('.JS-rorate-10').toggleClass('active');
+    $('.JS-hide-10').slideToggle();
+});
+$('.JS-click-11').click(function() {
+    $('.JS-rorate-11').toggleClass('active');
+    $('.JS-hide-11').slideToggle();
+});
+$('.JS-click-12').click(function() {
+    $('.JS-rorate-12').toggleClass('active');
+    $('.JS-hide-12').slideToggle();
+});
+$('.JS-click-13').click(function() {
+    $('.JS-rorate-13').toggleClass('active');
+    $('.JS-hide-13').slideToggle();
+});
+$('.JS-click-14').click(function() {
+    $('.JS-rorate-14').toggleClass('active');
+    $('.JS-hide-14').slideToggle();
+});
+$('.JS-click-15').click(function() {
+    $('.JS-rorate-15').toggleClass('active');
+    $('.JS-hide-15').slideToggle();
+});
+$('.JS-click-16').click(function() {
+    $('.JS-rorate-16').toggleClass('active');
+    $('.JS-hide-16').slideToggle();
+});
+$('.JS-click-17').click(function() {
+    $('.JS-rorate-17').toggleClass('active');
+    $('.JS-hide-17').slideToggle();
+});
+});
---
+++ v2.1.0_add-files_210401/page-comp.php
@@ -0,0 +1,7 @@
+<?php
+//header("HTTP/1.1 301 Moved Permanently");
+//header("Location: https://sppo-lp.net/");
+?>
+<?php session_start(); ?>
+<?php include_once ('page-mail_user.php'); ?>
+<?php include_once ('page-mail.php'); ?>
