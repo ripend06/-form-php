b5f6f6ebc498f194361af5470392c0c99a98b57c
ripend06 <makoto6760@gmail.com>
2021-04-02 15:12:57 +0900

    v3.1.1_add-reCAPTCH_210402

--- v3.0.0_add-reCAPTSHA-210402/page-comp.php
+++ v3.0.0_add-reCAPTSHA-210402/page-comp.php
@@ -1,7 +1,31 @@
 <?php
+
+//リダイレクト
 //header("HTTP/1.1 301 Moved Permanently");
 //header("Location: https://sppo-lp.net/");
+
+
+if (isset($_POST["recaptchaResponse"]) && !empty($_POST["recaptchaResponse"]))
+{
+    $secret = "6LeZ3ZgaAAAAAC5CTOIKSZLPcGX-LT-uFhFeMS7B";
+    $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$_POST["recaptchaResponse"]);
+    $reCAPTCHA = json_decode($verifyResponse);
+    if ($reCAPTCHA->success)
+    {
+      //echo "認証成功";
+      session_start();
+      include_once ('page-mail_user.php');
+      include_once ('page-mail.php');
+    }
+    else
+    {
+      echo "認証エラー";
+    }
+}
+else
+{
+    echo "エラーエラー";
+}
+
+
 ?>
-<?php session_start(); ?>
-<?php include_once ('page-mail_user.php'); ?>
-<?php include_once ('page-mail.php'); ?>
